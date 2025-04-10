4a655959943edaa133444a831c30ec689df1a52e
a7medal <ahmedalahmedbedi@gmail.com>
2025-04-10 20:35:59 +0000

    Create serviceWorker.js

---
+++ serviceWorker.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'ahmedal-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/main.js',
+  '/icon.png'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
